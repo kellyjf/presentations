d057b6cebcb3c170e1d6fc69e2165c4a3d2bb21a
John Kelly <kellyjf@yahoo.com>
2018-10-04 19:47:48 +0000

    Dictionary Attack

--- debugging/python/dict.py
+++ debugging/python/dict.py
@@ -1,17 +1,25 @@
 #!/usr/bin/python
 
 import string
+import md5
+import signal
+import pdb
+import itertools
+
+def handler(*args):
+	pdb.set_trace()
+
+signal.signal(signal.SIGUSR1, handler)
 
 cnt=0
 d=dict()
 with open("/usr/share/dict/words","r") as f:
 	for line in f:
 		line.strip()
-		d[cnt%1000]=line
-		print line
-		for i in range(1000):
-			entry=d.get(i,"")
-			if line==entry:
-				print i,line,entry,line==entry
+		perms=[[string.lower(x),string.upper(x)] for x in line]
+		for trial in itertools.product(*perms):
+			camel="".join(trial)
+			if md5.new(camel).hexdigest()=="d827509c155e76332260e853537ca58a":
+				print "Found it!",line,camel
 
 
